314358320d19d3db7f826dcbe11e01c5b3649a1f
Viking5776 <Viking5776@yandex.ru>
2023-03-22 20:17:56 +0300

    Add Seminar5

---
+++ Seminar5/Seminar5.py
@@ -0,0 +1,17 @@
+### Задача 26
+
+# def exponent(a, b):
+#     if b == 0: return 1
+#     elif b > 0: return(a*exponent(a,b-1))
+#     else: return(1/(a*exponent(a,-b-1)))
+
+# print(exponent(int(input("Введите число: ")), int(input("Введите степень: "))))
+
+
+### Задача 28
+
+# def sum(a, b):
+#     if b == 0: return a
+#     else: return(sum(a, b-1)+1)
+
+# print(sum(int(input("Введите число А: ")), int(input("Введите число В: "))))
