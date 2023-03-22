b0d7163e9b8481399540a6accd2e4ed3bc7bb6a0
Viking5776 <Viking5776@yandex.ru>
2023-03-22 20:50:24 +0300

    Add Seminar6

---
+++ Seminar6/Seminar6.py
@@ -0,0 +1,22 @@
+import random
+
+### Задача 30
+
+# a = int(input("Введите первое число арифметической прогрессии: "))
+# d = int(input("Введите разность элементов арифметической прогрессии: "))
+# l = int(input("Введите длину (количество элементов) арифметической прогрессии: "))
+# progression = [a+(n-1)*d for n in range(1, l+1)]
+
+# print(progression)
+
+
+### Задача 32
+
+# array = [random.randint(-10, 10) for i in range(20)]
+# print(array)
+
+# min = int(input("Введите нижнюю границу диапазона: "))
+# max = int(input("Введите верхнюю границу диапазона: "))
+
+# index_array = [i for i in range(len(array)) if array[i] >= min and array[i] <= max]
+# print(index_array)
